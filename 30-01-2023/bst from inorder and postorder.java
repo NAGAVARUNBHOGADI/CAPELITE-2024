50007a1642ea9aa33c5a8d21a1362be69e8d9b28
NAGA VARUN BHOGADI <85406841+NAGAVARUNBHOGADI@users.noreply.github.com>
2023-01-30 19:16:37 +0530

    Create bst from inorder and postorder.java

---
+++ 30-01-2023/bst from inorder and postorder.java
@@ -0,0 +1,19 @@
+    public TreeNode buildTree(int[] inorder, int[] postorder) {
+        HashMap<Integer ,Integer> map = new HashMap<>();
+        for(int i = 0 ; i<inorder.length ; i++){
+            map.put(inorder[i] , i);
+        }
+        
+        return create(inorder , postorder,0 , postorder.length - 1 , 0 , inorder.length-1 ,map);
+    }
+    public TreeNode create(int[] inorder , int[] postorder , int poststart , int postend , int instart , int inend , HashMap<Integer , Integer> map ){
+        if(instart > inend || poststart > postend){
+            return null;
+        }
+        int pos = map.get(postorder[postend]);
+        TreeNode root = new TreeNode(postorder[postend]);
+        root.left = create(inorder , postorder,poststart ,poststart+pos-instart-1,instart , pos-1 , map);
+        root.right = create(inorder , postorder,poststart +pos-instart ,postend-1 , pos+1 , inend, map);
+        return root;
+    }
+}
